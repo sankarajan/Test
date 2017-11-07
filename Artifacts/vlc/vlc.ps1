cee225c3de8aefc33431c433cd0262799b725f98
sivashankaran.r <sivashankaran.r@g7cr.in>
2017-11-07 17:18:14 +0530

    test

---
+++ Artifacts/vlc/vlc.ps1
@@ -0,0 +1,28 @@
+
+$workdir = "D:\installer\"
+# Check if work directory exists if not create it
+If (Test-Path -Path $workdir -PathType Container)
+{ Write-Host "$workdir already exists" -ForegroundColor Red}
+ELSE
+{ New-Item -Path $workdir  -ItemType directory }
+# Download the installer
+$source = "https://storageaccount4321.blob.core.windows.net/testsoftware/vlc-2.2.6-win32.exe"
+$destination = "$workdir\vlc-2.2.6-win32.exe"
+# Check if Invoke-Webrequest exists otherwise execute WebClient
+if (Get-Command 'Invoke-Webrequest')
+{
+     Invoke-WebRequest $source -OutFile $destination
+}
+else
+{
+    $WebClient = New-Object System.Net.WebClient
+    $webclient.DownloadFile($source, $destination)
+}
+Invoke-WebRequest $source -OutFile $destination 
+# Start the installation
+start-process "$workdir\vlc-2.2.6-win32.exe" /S  
+# Wait XX Seconds for the installation to finish
+Start-Sleep -s 35
+# Remove the installer
+rm -Force $workdir\7*
+ 
